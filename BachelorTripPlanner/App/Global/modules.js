88aae2810972ca5483659051a7cc7823cabae6d3
Radu <rdenis9616@gmail.com>
2019-02-11 16:08:10 +0200

    Change bootstrap 4.3 to 3.3.7

--- BachelorTripPlanner/App/Global/modules.js
+++ BachelorTripPlanner/App/Global/modules.js
@@ -1,24 +1,21 @@
-﻿
+﻿var globalModule = angular.module('globalModule', [
+    // Angular modules
+    'ngRoute', 'ngAnimate', 'ngCookies', 'ngResource', 'ngSanitize', 'ngTouch', 'ui.bootstrap'
 
-    var globalModule = angular.module('globalModule', [
-        // Angular modules
-        'ngRoute', 'ngAnimate', 'ngCookies', 'ngResource', 'ngSanitize', 'ngTouch'
+]);
 
-    ]);
-
-    globalModule.config(function ($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'AppViews/Home/home.html',
-                controller: 'HomeController'
-            })
-            .when('/home', {
-                templateUrl: 'AppViews/Home/home.html',
-                controller: 'HomeController'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-        $locationProvider.html5Mode(true);
-
-    });
+globalModule.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'AppViews/Home/home.html',
+            controller: 'HomeController'
+        })
+        .when('/home', {
+            templateUrl: 'AppViews/Home/home.html',
+            controller: 'HomeController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+    $locationProvider.html5Mode(true);
+});
