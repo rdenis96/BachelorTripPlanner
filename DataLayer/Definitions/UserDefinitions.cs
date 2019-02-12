de76b28025ff09d4835f51bb9c70e8c90c07f94d
Radu <rdenis9616@gmail.com>
2019-02-12 22:25:16 +0200

    Add DataLayer with User model and repository and dbcontext (untested)

---
+++ DataLayer/Definitions/UserDefinitions.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Definitions
+{
+    static internal class UserDefinitions
+    {
+        public static void Set(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(k => k.Id);
+            modelBuilder.Entity<User>().Property(p => p.Email).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<User>().Property(p => p.Password).IsRequired();
+        }
+    }
+}
---
+++ DataLayer/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DataLayer.Models
+{
+    public class User : IEquatable<User>
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            return Equals((User)obj);
+        }
+
+        public bool Equals(User obj)
+        {
+            if (obj == null)
+                return false;
+
+            var result = (Id == obj.Id) &&
+                         (Email.Equals(obj.Email)) &&
+                         (Password.Equals(obj.Password));
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+    }
+}
