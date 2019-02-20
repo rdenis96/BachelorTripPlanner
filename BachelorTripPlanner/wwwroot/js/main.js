90c45e97929b88deaf35ec8e3e2eaefa77c7ccf1
Radu Denis Florian <rdenis9616@gmail.com>
2019-02-20 09:04:14 +0200

    Wwroot not important

--- BachelorTripPlanner/wwwroot/js/main.js
+++ BachelorTripPlanner/wwwroot/js/main.js
@@ -1,26 +1,23 @@
 var globalModule = angular.module('globalModule', [
-        // Angular modules
-        'ngRoute', 'ngAnimate', 'ngCookies', 'ngResource', 'ngSanitize', 'ngTouch'
+    // Angular modules
+    'ngRoute', 'ngAnimate', 'ngCookies', 'ngResource', 'ngSanitize', 'ngTouch', 'ui.bootstrap'
+]);
 
-    ]);
-
-    globalModule.config(function ($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'AppViews/Home/home.html',
-                controller: 'HomeController'
-            })
-            .when('/home', {
-                templateUrl: 'AppViews/Home/home.html',
-                controller: 'HomeController'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-        $locationProvider.html5Mode(true);
-
-    });
-
+globalModule.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'AppViews/Home/home.html',
+            controller: 'HomeController'
+        })
+        .when('/home', {
+            templateUrl: 'AppViews/Home/home.html',
+            controller: 'HomeController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+    $locationProvider.html5Mode(true);
+});
 
 globalModule.factory('homeRepository', [
     '$resource',
